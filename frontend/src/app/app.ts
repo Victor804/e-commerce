40f0126fe8e57baf90900fc9863d21916ca220cb
victor804 <victor.vaucel@gmail.com>
2025-10-06 15:08:25 +0200

    feat(front): add search-bar component

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,22 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ProductCard } from './components/product-card/product-card';
 import { Product } from './models/product.model';
+import { SearchBar } from './components/search-bar/search-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [ProductCard],
+  imports: [CommonModule, ProductCard, SearchBar],
   templateUrl: 'app.html',
   styleUrl: 'app.css',
 })
 export class App {
-  product1!: Product;
-  product2!: Product;
+  products!: Product[];
+
+  search = '';
 
   constructor() {
-    this.product1 = new Product();
-    this.product2 = new Product();
-    this.product2.name = 'Smartphone';
-    this.product2.price = 599.99;
-    this.product2.description = '6.5-inch display, 128GB storage, 5G enabled';
+    this.products = [];
+
+    const product1 = new Product();
+    this.products.push(product1);
+
+    const product2 = new Product();
+    product2.name = 'Smartphone';
+    product2.price = 599.99;
+    product2.description = '6.5-inch display, 128GB storage, 5G enabled';
+    this.products.push(product2);
+
+    const product3 = new Product();
+    product3.name = 'Wireless Headphones';
+    product3.price = 199.99;
+    product3.description = 'Noise-cancelling over-ear headphones';
+    this.products.push(product3);
   }
 }
---
+++ frontend/src/app/components/search-bar/search-bar.ts
@@ -0,0 +1,12 @@
+import { Component, EventEmitter, model, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-search-bar',
+  imports: [FormsModule],
+  templateUrl: './search-bar.html',
+  styleUrl: './search-bar.css',
+})
+export class SearchBar {
+  search = model<string>('');
+}
